1743c65d41ca56f32d2f86fcbbd5438d3cddf84d
Aneesh2007 <Aneesh@Aneesh.localdomain>
2025-12-07 17:34:19 +0000

    Added self closing brackets

--- init.lua
+++ init.lua
@@ -117,7 +117,22 @@ require("lazy").setup({
           }),
         })
       end,
-    }, 
+    },
+        -------------------------------------------------------
+    -- Auto pairs ( (), {}, [])
+    -------------------------------------------------------
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+        require("nvim-autopairs").setup({})
+        
+        -- Integrate with nvim-cmp
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end,
+    }, 
   },
 
   -- Which colorscheme to install by default
