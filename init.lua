4a34ba9d0db54005109160a45a29e61f42c10648
Aneesh2007 <Aneesh@Aneesh.localdomain>
2025-12-07 17:26:13 +0000

    Added auto-complete

--- init.lua
+++ init.lua
@@ -78,7 +78,46 @@ require("lazy").setup({
       config = function()
         require("telescope").setup({})
       end,
-    },
+    },
+       -------------------------------------------------------
+    -- Autocomplete (nvim-cmp)
+    -------------------------------------------------------
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+      },
+      config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+
+          mapping = cmp.mapping.preset.insert({
+            ["<Tab>"] = cmp.mapping.select_next_item(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+          }),
+
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "buffer" },
+            { name = "path" },
+          }),
+        })
+      end,
+    }, 
   },
 
   -- Which colorscheme to install by default
